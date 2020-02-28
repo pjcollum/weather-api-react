c6423ffdb6306ae81b31e4748587242c44864202
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-28 16:23:43 +0000

    first commit

---
+++ src/components/Weather.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+               {this.props.city}
+               {this.props.country}
+               {this.props.temperature}
+               {this.props.humidity}
+               {this.props.description}
+            </div>
+        )
+    }
+}
+
+export default Weather;
