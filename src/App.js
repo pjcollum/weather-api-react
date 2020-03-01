8d4671965f5a321d1cffa4c432cb4581bf9cd2fb
Pete <petercollum@hotmail.co.uk>
2020-03-01 18:46:07 +0000

    completed weather app api in react

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ class App extends React.Component {
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
 
     const data = await api_call.json();
-    console.log(data);
+    
+    if (city && country){     //both fields need to be filled to click button
+      console.log(data);
     
     this.setState({
       temperature: data.main.temp,
@@ -33,7 +35,17 @@ class App extends React.Component {
       description: data.weather[0].description,
       error: ""
     })
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value.",
+    });
     
+    }
   }
 
   render() {
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
- class Weather extends React.Component {
-    render() {
-        return (
-            <div>
-               {this.props.city}
-               {this.props.country}
-               {this.props.temperature}
-               {this.props.humidity}
-               {this.props.description}
-            </div>
-        )
-    }
-}
-
+const Weather = props => (
+        <div>
+                {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
+               {props.temperature && <p>Temperature: {props.temperature}</p>}
+               {props.humidity && <p>Humidity: {props.humidity}</p>}
+               {props.description && <p>Conditions: {props.description}</p>}
+               {props.error && <p>{props.error}</p>}
+               
+        </div>
+    
+)
 export default Weather;
